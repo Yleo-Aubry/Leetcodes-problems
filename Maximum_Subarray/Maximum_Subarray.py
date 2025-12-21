bae00e9ddc7b2597486c1bdecf7259279d3e5a32
Yleo-Aubry <yleo.aubry88@icloud.com>
2025-12-21 16:49:04 +0100

    Create Maximum_Subarray.py

---
+++ Maximum_Subarray/Maximum_Subarray.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        maxSum = nums[0]
+        curSum = 0
+        
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSum = max(maxSum, curSum)
+            
+        return maxSum
+
+
+
+        
+
+
+        
+        
+        
