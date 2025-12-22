332c88e9e90037ac2ceedadf7041ccd14ff31f3f
Yleo-Aubry <yleo.aubry88@icloud.com>
2025-12-22 12:25:32 +0100

    Create Min_Stack.py

---
+++ Min_Stack/Min_Stack.py
@@ -0,0 +1,36 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        val_to_push = val
+        if self.minStack:
+            val_to_push = min(val, self.minStack[-1])
+            
+        self.minStack.append(val_to_push)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
